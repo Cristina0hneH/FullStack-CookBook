f2a93e7eb0804ab262ae2feb7f916c9627a9b384
Benedikt Kaltenecker <benedikt@kaltenecker.org>
2022-09-19 11:13:21 +0200

    deleted

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,27 @@
 import axios from "axios";
 import './App.css';
-import {useState, useEffect} from "react";
+import { useState, useEffect } from "react";
+
 
 
 function App() {
-  const [recipe, setRecipe] = useState([]); 
-  useEffect(()=>{
+  const [recipe, setRecipe] = useState([]);
+  useEffect(() => {
 
     axios.get('http://localhost:3001/recipes')
-    .then((res)=>{console.log(res)})
-    .catch((err)=>{console.log(err)})
-  },[])
+      .then((res) => {
+        console.log(res)
+        setRecipe(res.data.items)
+      })
+      .catch((err) => { console.log(err) })
+  }, [])
   return (
     <div className="App">
-      
+      {
+        console.log(recipe)
+
+      }
+
     </div>
   );
 }
--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const data = require("./data.json")
-const port = process.env.PORT || 3001; 
+const port = process.env.PORT || 3001;
 
+app.use(cors());
 console.log(data);
+
+app.get("/recipes", (req, res) => {
+  res.json(data)
+})
 
 app.listen(port, () => {
   console.log(`Hello, welcome to port no. ${port}`)
