c288ab3d3fb67db307e8b60aa31a857a7d0a2909
Cristina Caran <cristinacaran@Cristinas-Air.fritz.box>
2022-09-19 16:08:29 +0200

    mapped in home.js. Line 10 is for Ben to display the picture

--- client/src/App.js
+++ client/src/App.js
@@ -6,25 +6,20 @@ import Recipe from './components/recipe/Recipe';
 
 
 function App() {
-  const [recipe, setRecipe] = useState([]);
+  const [recipes, setRecipes] = useState([]);
   useEffect(() => {
 
     axios.get('http://localhost:3001/recipes')
       .then((res) => {
-        console.log(res)
-        setRecipe(res.data.items)
+        setRecipes(res.data) 
       })
       .catch((err) => { console.log(err) })
   }, [])
+  
   return (
     <div className="App">
-      <Home />
+      <Home recipes={recipes}/>
       <Recipe />
-      {
-        console.log(recipe)
-
-      }
-
 
     </div>
   );
--- index.js
+++ index.js
@@ -2,8 +2,7 @@ const express = require('express')
 const cors = require('cors')
 const app = express()
 const data = require("./data.json")
-const port = process.env.PORT || 3001; 
-const cors = require("cors");
+const port = process.env.PORT || 3001;
 
 app.use(cors());
 app.get('/recipes', (req, res) => {
@@ -19,15 +18,6 @@ const  id = req.params.id;
     return recipe.sys.id === id;
   })
   res.json(recipe)
-})
-const port = process.env.PORT || 3001;
-
-app.use(cors());
-console.log(data);
-
-
-app.get("/recipes", (req, res) => {
-  res.json(data)
 })
 
 app.listen(port, () => {
